382a4c6675c12359c1fea52c1fd6c78f471b3a6f
David Zucker <davzucky@hotmail.com>
2021-09-15 20:02:49 +0800

    Update following review

--- start_prefect_server.sh
+++ start_prefect_server.sh
@@ -4,5 +4,5 @@ prefect server start --use-volume --use-volume --detach
 export PREFECT__BACKEND=server
 export PREFECT__CLOUD__ENDPOINT=http://localhost
 echo "==> Create tenant market"
-prefect server create-tenant --name market
+prefect server create-tenant --name market > /dev/null 2>&1
 prefect create project --skip-if-exists market_data
